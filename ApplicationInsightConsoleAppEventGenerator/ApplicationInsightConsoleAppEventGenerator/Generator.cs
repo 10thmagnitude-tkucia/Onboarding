3dff2fa08269ac85468318266e48d55a54c65506
10thmagnitude-tkucia <32137569+10thmagnitude-tkucia@users.noreply.github.com>
2017-09-22 10:46:18 -0500

    Chceking in some stuff

---
+++ ApplicationInsightConsoleAppEventGenerator/ApplicationInsightConsoleAppEventGenerator/Generator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+
+namespace ApplicationInsightConsoleAppEventGenerator
+{
+    class Generator
+    {
+        static int generatorId;
+        private TelemetryClient telemetryClient;
+        private Random random;
+        private Thread generatorThread;
+        private int highLimit;
+        private int instanceId;
+        public Generator(TelemetryClient tc, int timerHighLimit)
+        {
+            telemetryClient = tc;
+            instanceId = generatorId++;
+            random = new Random();
+            highLimit = timerHighLimit;
+            generatorThread = new Thread(new ThreadStart(RunProcess));
+        }
+
+        private bool runProcess = false;
+
+        public int ThreadSleep
+        {
+            get; set;
+        } = 3000;
+
+        public void Stop()
+        {
+            runProcess = false;
+            generatorThread.Abort();
+        }
+
+        public void Start()
+        {
+            runProcess = true;
+            generatorThread.Start();
+        }
+
+        private void RunProcess()
+        {
+
+            while (runProcess == true)
+            {
+                ThreadSleep = random.Next(10000, highLimit);
+
+                telemetryClient.TrackEvent("ProcessRan" + instanceId);
+                Console.WriteLine(instanceId + "Sent TrackEvent: " + DateTime.Now);
+
+                if (ThreadSleep % 10000 > 0.0)
+                {
+                    telemetryClient.TrackEvent("FunkyNumber");
+                    Console.WriteLine(instanceId + "FunkyNumber");
+                }
+
+                Thread.Sleep(ThreadSleep);
+            }
+        }
+
+    }
+}
---
+++ ApplicationInsightConsoleAppEventGenerator/ApplicationInsightConsoleAppEventGenerator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+
+namespace ApplicationInsightConsoleAppEventGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var ApplicationInsightKey = "2220d365-51c7-472d-9921-0ee91d4f435a";
+
+            var tc = new TelemetryClient() { InstrumentationKey = ApplicationInsightKey };
+            // Set session data:
+            tc.Context.User.Id = Environment.UserName;
+            tc.Context.Session.Id = Guid.NewGuid().ToString();
+            tc.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+            var generator1 = new Generator(tc, 30000);
+            generator1.Start();
+
+            var generator2 = new Generator(tc, 45000);
+            generator2.Start();
+
+            var generator3 = new Generator(tc, 60000);
+            generator3.Start();
+
+            var generator4 = new Generator(tc, 72000);
+            generator4.Start();
+
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+
+            generator1.Stop();
+            generator2.Stop();
+            generator3.Stop();
+            generator4.Stop();
+        }
+
+
+
+    }
+
+}
